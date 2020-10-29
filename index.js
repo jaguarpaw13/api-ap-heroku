73a1987b9e8461554d7082c2faa309af60ce7fb1
jaguarpaw13 <dannytalboom@gmail.com>
2020-10-29 11:43:17 +0100

    Update index.js
    Test cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://www.portasecura.com'
+        origin: 'https://dannytalboom13.herokuapp.com/'
     })
 );
 app.options('*', cors());
